cde5da0a6ceb754a7c8d9d5a457509ad5b16d35b
emmapereira <emmapereira23@gmail.com>
2023-03-26 19:13:34 +0200

    user input page done

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 
 function setup() {
-  createCanvas(320, 480);
+  //createCanvas(320, 480);
 }
 
 function draw() {
   //Set a nice yellow background
-  background(255, 255, 0);
+  //background(255, 255, 0);
 }
 
